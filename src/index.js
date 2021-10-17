70a53fa4b901445b0ac822f10f0379b4b2e441f6
Barbara <barbarabs1703@gmail.com.br>
2021-10-17 21:58:32 -0200

    email feito

--- src/index.js
+++ src/index.js
@@ -355,5 +355,36 @@ app.delete('/agendamento/:id', async (req, resp) => {
 
 
 
+
+
+
+app.post('/enviar', async (req, resp) => {
+    try {
+        const response = await
+        enviarEmail(req.body.para, req.body.assunto, req.body.mensagem);
+
+        resp.send(response);
+
+    } catch(e) {
+        resp.send(e)
+    }
+})
+
+app.get('/buscarbairro', async (req, resp) => {
+    try{
+        const api_key = 'b866c3722fa645f9acb1da4674663672';
+        const { lat, lon } = req.query;
+
+        let r = await axios.get(`https://api.geoapify.com/v1/geocode/reverse?lat=${lat}&lon=${lon}&apiKey=${api_key}`);
+        resp.send(r.data);
+
+    } catch(e) {
+        resp.send(e);
+    }
+})
+
+
+
+
 app.listen(process.env.PORT,
     x => console.log(`>> Server up at port ${process.env.PORT}`))
